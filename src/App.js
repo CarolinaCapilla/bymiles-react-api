597b0ee579ba7b0e7befb90fc83ceef7cad66e9d
CarolinaCapilla <carolina.capilla@gmail.com>
2021-01-18 12:43:03 +0000

    created Login and PolicyDetails components with state (at the moment empty) which it will then by filled by API data, tested rendering both components and they render fine so far

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
-import './App.css';
+
+import Login from './Components/Login/Login'
+import PolicyDetails from './Components/PolicyDetails/PolicyDetails'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
+      <Login />
+      <PolicyDetails />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/PolicyDetails/PolicyDetails.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class PolicyDetails extends Component {
+  state = {
+    policyReference: '',
+    coverType: '',
+    car: '',
+    address: '',
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hi again</h1>
+      </div>
+    )
+  }
+}
+
+export default PolicyDetails;
