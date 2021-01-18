544275613a8933718d6cf6bb320ada8dc89c53d5
CarolinaCapilla <carolina.capilla@gmail.com>
2021-01-18 18:15:16 +0000

    login component desgin done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react'
 
 import Login from './Components/Login/Login'
-import PolicyDetails from './Components/PolicyDetails/PolicyDetails'
+// import PolicyDetails from './Components/PolicyDetails/PolicyDetails'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <Login />
-      <PolicyDetails />
     </div>
   )
 }
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
+import './Login.css'
 
+import { Avatar, Button, Grid, Paper, TextField } from '@material-ui/core'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import purple from '@material-ui/core/colors/purple'
 class Login extends Component {
   state = {
     username: '',
@@ -8,11 +12,40 @@ class Login extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Hi</h1>
-      </div>
+      <Grid>
+        <Paper elevation={10} className="paperStyle">
+          <Grid align="center">
+            <Avatar style={{ backgroundColor: purple[200] }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <h2>Sign in</h2>
+            <Grid className="textInput">
+              <TextField
+                fullWidth
+                id="standard-basic"
+                label="User Name"
+                placeholder="Enter User Name"
+                required
+              ></TextField>
+              <Grid className="textInput">
+                <TextField
+                  fullWidth
+                  id="standard-basic"
+                  label="Password"
+                  placeholder="Enter Password"
+                  type="password"
+                  required
+                ></TextField>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Button fullWidth type="submit" variant="contained" color="primary">
+            Sign in
+          </Button>
+        </Paper>
+      </Grid>
     )
   }
 }
 
-export default Login;
+export default Login
