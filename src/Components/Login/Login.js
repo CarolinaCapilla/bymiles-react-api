ba2974a59d66686676dfdac7dfdb9a29149e8f8f
CarolinaCapilla <carolina.capilla@gmail.com>
2021-01-19 15:44:15 +0000

    created postDataHandler method that takes care of the POST request to the api and sets the state of type to the access_token received as a response to then use it for our GET request to the Policy Details endpoint

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import './Login.css';
+import axios from 'axios';
 
+import './Login.css';
 import { Avatar, Button, Grid, Paper, TextField } from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import purple from '@material-ui/core/colors/purple';
@@ -8,6 +9,32 @@ class Login extends Component {
   state = {
     username: '',
     password: '',
+    type: '',
+  };
+
+  postDataHandler = () => {
+    const userLogin = {
+      username: this.state.username,
+      password: this.state.password,
+      type: 'USER_PASSWORD_AUTH',
+    };
+    const headers = {
+      environment: 'mock',
+      'Content-type': 'application/json',
+    };
+    axios
+      .post('https://api.bybits.co.uk/auth/token', userLogin, {
+        headers: headers,
+      })
+      .then((response) => {
+        console.log(response);
+        if (response.status === 200) {
+          console.log('Login successfull');
+        }
+        this.setState({
+          type: response.data.access_token,
+        });
+      });
   };
 
   render() {
@@ -22,7 +49,6 @@ class Login extends Component {
             <Grid className="textInput">
               <TextField
                 fullWidth
-                id="standard-basic"
                 label="User Name"
                 placeholder="Enter User Name"
                 required
@@ -30,7 +56,6 @@ class Login extends Component {
               <Grid className="textInput">
                 <TextField
                   fullWidth
-                  id="standard-basic"
                   label="Password"
                   placeholder="Enter Password"
                   type="password"
@@ -39,7 +64,13 @@ class Login extends Component {
               </Grid>
             </Grid>
           </Grid>
-          <Button fullWidth type="submit" variant="contained" color="primary">
+          <Button
+            onClick={this.postDataHandler}
+            fullWidth
+            type="submit"
+            variant="contained"
+            color="primary"
+          >
             Sign in
           </Button>
         </Paper>
