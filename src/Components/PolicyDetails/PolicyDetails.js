e70fa8a453b5c3f4b9fc8c6fda9f5defbf191fe8
CarolinaCapilla <carolina.capilla@gmail.com>
2021-01-20 20:34:03 +0000

    PolicyDetails now display the correct fetched data from the API, needs formatting to match the requirements

--- src/Components/PolicyDetails/PolicyDetails.js
+++ src/Components/PolicyDetails/PolicyDetails.js
@@ -6,40 +6,43 @@ import { Grid, Paper, Card, CardContent, Typography } from '@material-ui/core';
 import purple from '@material-ui/core/colors/purple';
 import grey from '@material-ui/core/colors/grey';
 
-const initialState = {
-  policyDetails: [],
-  isFetching: false,
-  hasError: false,
-  errorMessage: null,
-};
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'FETCH_POLICY_REQUEST':
-      return {
-        ...state,
-        isFetching: true,
-        hasError: false,
-      };
-    case 'FETCH_POLICY_SUCCESS':
-      return {
-        ...state,
-        isFetching: false,
-        songs: action.payload,
-      };
-    case 'FETCH_POLICY_FAILED':
-      return {
-        ...state,
-        hasError: true,
-        isFetching: false,
-      };
-    default:
-      return state;
-  }
-};
 export const PolicyDetails = () => {
+  const initialState = {
+    policyRef: '',
+    coverType: '',
+    car: '',
+    address: '',
+    isFetching: false,
+    hasError: false,
+    errorMessage: null,
+  };
   const { state: authState } = React.useContext(AuthContext);
-  const [state, dispatch] = React.useReducer(reducer, initialState);
-  // const [data, setData] = React.useState(initialState);
+  const [data, setData] = React.useState(initialState);
+  const reducer = (data, action) => {
+    switch (action.type) {
+      case 'FETCH_POLICY_REQUEST':
+        return {
+          ...data,
+          isFetching: true,
+          hasError: false,
+        };
+      case 'FETCH_POLICY_SUCCESS':
+        return {
+          ...data,
+          isFetching: false,
+          songs: action.payload,
+        };
+      case 'FETCH_POLICY_FAILED':
+        return {
+          ...data,
+          hasError: true,
+          isFetching: false,
+        };
+      default:
+        return data;
+    }
+  };
+  const [, dispatch] = React.useReducer(reducer, initialState);
   React.useEffect(() => {
     dispatch({
       type: 'FETCH_POLICY_REQUEST',
@@ -60,11 +63,24 @@ export const PolicyDetails = () => {
         }
       })
       .then((resJson) => {
-        console.log(resJson);
+        setData({
+          policyRef: resJson.policy.policy_ref,
+          coverType: resJson.policy.cover,
+          car:
+            resJson.vehicle.make +
+            resJson.vehicle.model +
+            resJson.vehicle.colour +
+            resJson.vehicle.reg,
+          address:
+            resJson.policy.address.line_1 +
+            resJson.policy.address.line_2 +
+            resJson.policy.address.postcode,
+        });
         dispatch({
           type: 'FETCH_POLICY_SUCCESS',
           payload: resJson,
         });
+        console.log(resJson);
       })
       .catch((error) => {
         dispatch({
@@ -72,7 +88,6 @@ export const PolicyDetails = () => {
           errorMessage: error,
         });
       });
-    console.log(state);
   }, [authState.access_token]);
 
   return (
@@ -83,15 +98,11 @@ export const PolicyDetails = () => {
           <Grid>
             <Card style={{ border: 'none', boxShadow: 'none' }}>
               <CardContent>
-                <Typography
-                  variant="h6"
-                  style={{ color: grey[900] }}
-                  policyref={state.policyDetails}
-                >
+                <Typography variant="h6" style={{ color: grey[900] }}>
                   Policy Reference:
                 </Typography>
                 <Typography style={{ color: grey[600] }}>
-                  {state.policyRef}
+                  {data.policyRef}
                 </Typography>
               </CardContent>
               <CardContent>
@@ -99,20 +110,22 @@ export const PolicyDetails = () => {
                   Cover Type:
                 </Typography>
                 <Typography style={{ color: grey[600] }}>
-                  {state.coverType}
+                  {data.coverType}
                 </Typography>
               </CardContent>
               <CardContent>
                 <Typography variant="h6" style={{ color: grey[900] }}>
                   Car:
                 </Typography>
-                <Typography style={{ color: grey[600] }}></Typography>
+                <Typography style={{ color: grey[600] }}>{data.car}</Typography>
               </CardContent>
               <CardContent>
                 <Typography variant="h6" style={{ color: grey[900] }}>
                   Address:
                 </Typography>
-                <Typography style={{ color: grey[600] }}></Typography>
+                <Typography style={{ color: grey[600] }}>
+                  {data.address}
+                </Typography>
               </CardContent>
             </Card>
           </Grid>
